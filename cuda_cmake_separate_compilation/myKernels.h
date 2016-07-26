ade2f54aa62e6b00b7b1b55ed2c5fee48140ea66
khalilBF <khalil.ben.fadhel.1@gmail.com>
2016-07-26 15:34:40 -0400

    Update myKernels.h

--- cuda_cmake_separate_compilation/myKernels.h
+++ cuda_cmake_separate_compilation/myKernels.h
@@ -1,16 +1,10 @@
-/*
- * myKernels.h
- *
- *  Created on: 2016-07-22
- *      Author: khalil
- */
-
 #ifndef MYKERNELS_H_
 #define MYKERNELS_H_
+
+// the kernel wrapper
 float *gpuReciprocal(float *data, unsigned size);
 
+// some cpu functions for validation
 float *cpuReciprocal(float *data, unsigned size);
 void initialize(float *data, unsigned size);
 #endif /* MYKERNELS_H_ */
-
-
